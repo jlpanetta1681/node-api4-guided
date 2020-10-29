08791f5e71dc644451f569e4af8bb464cc8bf38f
Joe_Panetta <jlpanetta1681@icloud.com>
2020-10-29 13:02:06 -0400

    deployment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const server = require("./api/server.js");
+require('dotenv').config();
+const server = require('./api/server.js');
+// only local
 
-const port = 5000;
+const port = process.env.PORT;
+const secret = process.env.SECRET_THING;
+
+console.log(port, secret);
 server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+	console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
